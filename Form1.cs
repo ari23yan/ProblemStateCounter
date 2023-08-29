9bd5c8e94815657a6dd30f99d99e2cc2830e4f4b
ariyan rahmani <ariyan137923@yahoo.com>
2023-08-29 11:40:57 +0330

    20230829-LastPush

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ProblemStateCounter
 {
@@ -15,6 +16,9 @@ namespace ProblemStateCounter
         public Form1()
         {
             InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,33 +31,57 @@ namespace ProblemStateCounter
             string input = textBox1.Text;
             if(input.Length < 45  || string.IsNullOrEmpty(input))
             {
+                label1.Visible = true;
                 label1.ForeColor = Color.Red;
                 label1.BackColor = Color.White;
                 label1.Text = "مقدار ورودی اشتباه می باشد";
             }
             else
             {
-                var problemStateCount = 0;
-                if(int.Parse(input.Substring(0,5)) != 0)
+                List<string> termsList = new List<string>();
+
+                if (int.Parse(input.Substring(0, 5)) != 0 || int.Parse(input.Substring(27, 7)) != 0)
                 {
-                    problemStateCount++; // alayandegi
+                    //techProblems = "آلایندگی" + " | ";
+                    termsList.Add("آلایندگی");
                 }
-                if (int.Parse(input.Substring(5,9 )) != 0 || int.Parse(input.Substring(20, 2)) != 0)
+                if (int.Parse(input.Substring(5, 9)) != 0 || int.Parse(input.Substring(20, 2)) != 0)
                 {
-                    problemStateCount++; // tormoz
+                    //techProblems += "ترمز" + " | ";
+                    termsList.Add("ترمز");
+
                 }
                 if (int.Parse(input.Substring(14, 6)) != 0)
                 {
-                    problemStateCount++; // komak fanar
+                    //techProblems += "کمک فنر" + " | ";
+                    termsList.Add("کمک فنر");
                 }
                 if (int.Parse(input.Substring(22, 2)) != 0)
                 {
-                    problemStateCount++; // laghzesh
+                    //techProblems += "لغزش جانبی" + " | ";
+                    termsList.Add("لغزش جانبی");
+
                 }
-                if (int.Parse(input.Substring(22, 2)) != 0)
+                if (int.Parse(input.Substring(35, 7)) != 0 || int.Parse(input.Substring(43, 3)) != 0)
                 {
-                    problemStateCount++; // laghzesh
+                    //techProblems += "ظاهری به جز صوت" + " | ";
+
+                    termsList.Add("ظاهری به جز صوت");
                 }
+                if (int.Parse(input.Substring(42, 1)) != 0)
+                {
+                    //techProblems += "فقط صوت " + " | ";
+
+                    termsList.Add("فقط صوت");
+                }
+                label2.Visible = true;
+                label2.Font = new Font("SansSerif", 24, FontStyle.Bold);
+                label2.ForeColor = Color.Green;
+                label2.BackColor = Color.White;
+                label2.Text =  termsList.Count().ToString();
+                label1.Visible= true;
+                label1.Font = new Font("IRANSans", 20, FontStyle.Bold);
+                label1.Text = string.Join(" / ", termsList);
 
 
             }
